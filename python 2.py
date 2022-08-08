df1036bc7cd1808b8527c9e22a83882ae11c53ba
fadlyhaqqiandrian <110508936+fadlyhaqqiandrian@users.noreply.github.com>
2022-08-07 18:51:15 -0700

    Add files via upload

---
+++ python 2.py
@@ -0,0 +1,13 @@
+#belajar output 
+
+nama = "FADLY" # fadly adalah data ,nama adalah variabel
+umur = 15
+alamat = "cimanggis, kota depok"
+
+print(nama)
+print(umur)
+print(alamat)
+
+print("nama saya adalah " + nama + " umur saya " + str (umur) +  " Alamat saya " + alamat )
+
+print("Nama saya adalah " + nama)
